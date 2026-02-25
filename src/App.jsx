36365410cb4a813427bcd575960b8b5282f217ea
Akil-Hub <akilanjummahi@gmail.com>
2026-02-25 23:29:43 +0600

    added some new fetures

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import Button from "./components/Button";
+import { AiFillBank } from "react-icons/ai";
+import CustomIcon from "./components/CustomIcon";
 
+import programming from'./assets/programing.jpg'
 const App = () => {
   return (
-    <div className="bg-gray-200 grid grid-cols-3 gap-10  p-10">
-      <Button title={"Click Me"} className="bg-green-400  border-none" />
-      <Button title={"Alert"} className="bg-amber-400  border-none" 
-      
-      onClick={()=> alert('Welcome to buttons gallery')}/>
-      <Button title={"Click Me"} className="bg-violet-400  border-none" />
-      <Button title={"Red btn"} className="bg-red-400  border-none" />
-      <Button title={"Blue Btn"} className="bg-blue-400  border-none" />
-    </div>
+    <>
+      <section className="flex wrapper justify-center items-center h-screen">
+        <div>
+          <div className="bg-amber-600 h-20 w-40 m-4 "><AiFillBank size={40} className="flex justify-center items-center mx-auto my-auto"/></div>
+          <div className="bg-amber-500 h-20 w-40 m-4 "><AiFillBank size={40} className="flex justify-center items-center mx-auto my-auto"/></div>
+          <div className="bg-amber-400 h-20 w-40 m-4 "><AiFillBank size={40} className="flex justify-center items-center mx-auto my-auto"/></div>
+          <div className="bg-amber-300 h-20 w-40 m-4 "><AiFillBank size={40} className="flex justify-center items-center mx-auto my-auto"/></div>
+          <div className="bg-amber-200 h-20 w-40 m-4 "><AiFillBank size={40} className="flex justify-center items-center mx-auto my-auto"/></div>
+        </div>
+        <div>
+          <div className="bg-blue-600 h-20 w-40 m-4 "> <CustomIcon/></div>
+          <div className="bg-blue-500 h-20 w-40 m-4 "> <CustomIcon/></div>
+          <div className="bg-blue-400 h-20 w-40 m-4 "> <CustomIcon/></div>
+          <div className="bg-blue-300 h-20 w-40 m-4 "> <CustomIcon/></div>
+          <div className="bg-blue-200 h-20 w-40 m-4 "> <CustomIcon/></div>
+        </div>
+        <div>
+          <div className="bg-green-600 h-20 w-40 m-4 "></div>
+          <div className="bg-green-500 h-20 w-40 m-4 "></div>
+          <div className="bg-green-400 h-20 w-40 m-4 "></div>
+          <div className="bg-green-300 h-20 w-40 m-4 "></div>
+          <div className="bg-green-200 h-20 w-40 m-4 "></div>
+        </div>
+        <div>
+          <div className="bg-red-600 h-20 w-40 m-4 my-20 "> <img src={programming} alt="" /> </div>
+          <div className="bg-red-500 h-20 w-40 m-4 my-20 "> <img src={programming} alt="" /> </div>
+          <div className="bg-red-400 h-20 w-40 m-4 my-20 "> <img src={programming} alt="" /> </div>
+          <div className="bg-red-300 h-20 w-40 m-4 my-20 "> <img src={programming} alt="" /> </div>
+          <div className="bg-red-200 h-20 w-40 m-4  my-20"> <img src={programming} alt="" /> </div>
+        </div>
+        <div>
+          <div className="bg-violet-600 h-20 w-40 m-4 "></div>
+          <div className="bg-violet-500 h-20 w-40 m-4 "></div>
+          <div className="bg-violet-400 h-20 w-40 m-4 "></div>
+          <div className="bg-violet-300 h-20 w-40 m-4 "></div>
+          <div className="bg-violet-200 h-20 w-40 m-4 "></div>
+        </div>
+      </section>
+    </>
   );
 };
 
